d882b30b6d40dc7724512253e67ffe8f23e1ff80
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-03-09 08:09:32 -0800

    Setting up route and model

--- server/models/photo.js
+++ server/models/photo.js
@@ -1,21 +1,10 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-var dogSchema = new mongoose.Schema({
-  dogName: String,
-  favoriteToy: String,
-  fixed: Boolean,
-  age: Number,
-  kibblePreference: { type: String, default: 'fish' }
+var photoSchema = new mongoose.Schema({
+  url: String,
+  caption: String
 });
 
-var Dog = mongoose.model('Dog', dogSchema);
-
-Dog.schema.path('kibblePreference').validate(function(value) {
-  return /fish|chicken|beef|mixed/i.test(value);
-}, 'Unavailable kibble match');
-
-module.exports = exports = mongoose.model('Dog', dogSchema);
+module.exports = exports = mongoose.model('Photo', photoSchema);
--- server/routes/photo_routes.js
+++ server/routes/photo_routes.js
@@ -2,53 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser').json();
-const Dog = require(__dirname + '/../models/dog');
+const Photo = require(__dirname + '/../models/photo');
 const handleDBError = require(__dirname + '/../lib/handle_db_error');
-const basicHTTP = require(__dirname + '/../lib/basic_http');
-const jwtAuth = require(__dirname + '/../lib/jwt_auth');
 
-var dogRouter = module.exports = exports = express.Router();
+var photoRouter = module.exports = exports = express.Router();
 
-dogRouter.use(function login(req, res, next) {
-  console.log('Database accessed at: ' + new Date() + '');
-  next();
-});
-
-dogRouter.get('/alldogs', (req, res) => {
-  Dog.find({}, (err, data) => {
+photoRouter.get('/photo', (req, res) => {
+  Photo.find({}, (err, data) => {
     if (err) return handleDBError(err, res);
     res.status(200).json(data);
   });
 });
 
-dogRouter.get('/myHuman', (req, res) => {
-  Human.count({}, (err, data) => {
+photoRouter.post('/photo', bodyParser, (req, res) => {
+  var newPhoto = new Photo(req.body);
+  newPhoto.save((err, data) => {
     if (err) return handleDBError(err, res);
     res.status(200).json(data);
-  });
-});
-
-// dogRouter.post('/dog', bodyParser, (req, res) => {
-//   var newDog = new Dog(req.body);
-//   newDog.save((err, data) => {
-//     if (err) return handleDBError(err, res);
-//
-//     res.status(200).json(data);
-//   });
-// });
-
-dogRouter.put('/dog/:id', bodyParser, (req, res) => {
-  var dogData = req.body;
-  delete dogData._id;
-  Dog.update({ _id: req.params.id }, dogData, (err, data) => {
-    if (err) return handleDBError(err, res);
-    res.status(200).json({ msg: 'Successfully updated dog' });
-  });
-});
-
-dogRouter.delete('/dog/:id', (req, res) => {
-  Dog.remove({ _id: req.params.id }, (err) => {
-    if (err) return handleDBError(err, res);
-    res.status(200).json({ msg: 'Successfully deleted dog' });
   });
 });
